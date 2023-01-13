116f130a96bf727154c7eb80d9f1722d786e6a2b
aowen7 <122053762+aowen7@users.noreply.github.com>
2023-01-13 12:16:41 -0800

    initial commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,13 +14,8 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-
-    public float horizontalInput;
-    public float speed = 10.0f;
-    public float xRange = 10.0f;
     void Update()
     {
-        //Keep the player in bounds
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
