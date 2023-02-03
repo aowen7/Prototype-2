305dc60e437714592a0e6b0e83c405269b5d6189
aowen7 <122053762+aowen7@users.noreply.github.com>
2023-02-03 12:35:43 -0800

    initial commit
    changed bottom limit

--- Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DestroyOutOfBoundsX : MonoBehaviour
 {
     private float leftLimit = -30;
-    private float bottomLimit = -5;
+    private float bottomLimit = -2;
 
     // Update is called once per frame
     void Update()
