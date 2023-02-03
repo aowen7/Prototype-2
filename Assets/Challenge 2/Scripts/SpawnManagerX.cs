f190652fc24f46eec89ddbbee9f779e7a629671e
aowen7 <122053762+aowen7@users.noreply.github.com>
2023-02-03 12:32:46 -0800

    initial commit
    displays Game Over!

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,12 +11,13 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        int spawnInterval = Random.Range(3, 5);
+        InvokeRepeating("SpawnRandomBall", startDelay, Random.Range(3, 5));
     }
 
     // Spawn random ball at random x position at top of play area
